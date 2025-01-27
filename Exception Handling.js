920a37a93d9daa74955bbafcf6796a43a9db7cf8
iamfaisalpk <iamfaizy123@gmail.com>
2025-01-27 16:23:19 +0530

    Handling

---
+++ Exception Handling.js
@@ -0,0 +1,21 @@
+// try
+
+// try {
+//     // code
+// }
+// catch (error){
+//     // catching error
+// }
+// finally {
+//     // cleanup
+// }
+
+// throw
+
+function validate(age){
+    if(age <0){
+        throw new Error(`ntha mone jadayanooh`);
+    }
+}
+
+validate(-24);
