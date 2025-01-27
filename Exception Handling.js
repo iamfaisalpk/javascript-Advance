74456d72040c24c5710051e0ce7095c8efeaf24a
iamfaisalpk <iamfaizy123@gmail.com>
2025-01-27 16:29:32 +0530

    changed

--- Exception Handling.js
+++ Exception Handling.js
@@ -12,10 +12,23 @@
 
 // throw
 
-function validate(age){
-    if(age <0){
-        throw new Error(`ntha mone jadayanooh`);
-    }
-}
+// function validate(age){
+//     if(age <0){
+//         throw new Error(`ntha mone jadayanooh`);
+//     }
+// }
 
-validate(-24);
+// validate(-24);
+
+try {
+    // Code that might throw an error
+    let result = 11 * 5;
+    console.log(result); // Works fine, but let's throw an error for demo
+    throw new Error("your thhe right but..");
+} catch (error) {
+    // Handles the error
+    console.log("An error occurred:", error.message);
+} finally {
+    // Runs no matter what
+    console.log(" complete.thanks");
+}
