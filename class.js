79c9e6bebfc34eafa720f2378ac580bb12525637
iamfaisalpk <iamfaizy123@gmail.com>
2025-01-29 10:40:33 +0530

    class

---
+++ class.js
@@ -0,0 +1,33 @@
+
+// class
+
+class Student{
+    name;
+    YOB;
+    place;
+    constructor(name,YOB,place){
+        this.name =name;
+        this.YOB = YOB;
+        this.place = place;
+    }
+
+    getAge(){
+        return new Date().getFullYear() -this.YOB;
+    };
+    getName(){
+        return this.name;
+    }
+    getPlace(){
+        return this.place;
+    }
+}
+
+// object
+
+var student1 = new Student("njan",2005,"kacheripadi");
+var student2 = new Student("iam",2004,"kacheripadi");
+var student3 = new Student("avan",2003,"kacheripadi");
+
+console.log(student1);
+console.log(student2);
+console.log(student3);
