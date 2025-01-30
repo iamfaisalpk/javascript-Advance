f4a786708bf362d0e344572fcd8635341f349a3d
iamfaisalpk <iamfaizy123@gmail.com>
2025-01-30 10:52:06 +0530

    callback

--- callback.js
+++ callback.js
@@ -9,3 +9,33 @@ function back(callback){
 }
 
 back(call);
+
+// callback hell
+
+console.log("start");
+
+function get(callback){
+    setTimeout(()=>{
+        callback(5);
+    },1000);
+}
+function get1(callback){
+    setTimeout(()=>{
+        callback(6);
+    },1000);
+}
+function get2(callback){
+    setTimeout(()=>{
+        callback(7);
+    },1000);
+}
+
+get((value)=>{
+    get1((get1Value)=>{
+        get2((get2Value)=>{
+            console.log(get1Value + get2Value + value);
+        })
+    })
+});
+
+console.log("end");
