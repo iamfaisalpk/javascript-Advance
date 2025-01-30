09ed25c37f5560271a5898b60ec4c3be552f9edf
iamfaisalpk <iamfaizy123@gmail.com>
2025-01-30 16:00:20 +0530

    async/await

---
+++ Async/Async/Await.js
@@ -0,0 +1,63 @@
+const promise1 = new Promise((res,rej)=>{
+    res(["java","java script"]);
+})
+
+// Promise1
+// .then((Response)=>{
+//     console.log(Response)
+// })
+// .catch((err)=>{
+//     console.log(err)
+// });
+
+    // function async
+
+// async function fetch(){
+// const Response = await Promise1;
+//     console.log(Response)
+// }
+
+// fetch
+
+            // Arrow function
+
+            
+// const  fetch = async () =>{
+//     const Response = await promise1;
+//         console.log(Response)
+//     };
+    
+//     fetch();
+
+//  Async/Await one methode
+
+// const fetch = async ()=>{
+//     const response = await promise1.catch((err)=>{
+//         console.log(err);
+//     });
+//     if (response){
+//         console.log(response);
+//     }
+
+// };
+
+// fetch()
+
+
+// try catch methode in async/await
+
+// const fetch = async () =>{
+//     try{
+//         const response = await promise1;
+//         console.log(response);
+//     }catch(error){
+//         console.log(error);
+//     }
+// };
+
+// fetch()
+
+
+        // pro tip
+
+
